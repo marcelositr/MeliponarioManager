de142638693a2563b6d83b21812e39681f2a668a
Marcelo Trindade <49993899+marcelositr@users.noreply.github.com>
2026-08-29 15:40:01 -0300

    feat: load report print styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./styles/enterprise.css";
 import "./styles/operations.css";
+import "./styles/reports.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
